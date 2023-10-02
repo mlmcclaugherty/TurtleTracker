a1e057b7865031bcd5a03d1f9ac64f800b964838
mlmcclaugherty <megan.mcclaugherty@duke.edu>
2023-10-02 17:46:05 -0400

    Revert "Switch for loop to while loop"
    This reverts commit a8d7bb97fbb2b53c418db6a46ef551e0cbfac558.

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -15,12 +15,16 @@ file_name = './data/raw/sara.txt'
 #Create a file object from the file. Giving Python access to the file's contents by opening it in memory
 file_object = open(file_name,'r')
 
-#Read contents of file into a string ling. This is a line list of all the lines in the Sara.txt data file.
-lineString= file_object.readline()
+#Read contents of file into a list. This is a line list of all the lines in the Sara.txt data file.
+line_list = file_object.readlines()
 
-while lineString:
+#Close the file. Once the contents of the file have been read into the memory done in the previous two lines of code, there's no need to have the file open.
+file_object.close()
+
+#For every iteration (line in the Sara.txt data file) it will update the variable lineString. 
+for lineString in line_list:
+    #Check if line is a data line (vs. just metadata type text found at the beginning of the Sara.txt doc). If the line starts with # or u, skip over it (continue).  
     if lineString[0] in ("#", "u"):
-        lineString = file_object.readline()
         continue
 
     #Split the string into a list of data items
@@ -35,6 +39,3 @@ while lineString:
 
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-
-    #Read next line
-    lineString= file_object.readline()
